cbcb5d83f58af52976a96f9fb738f0e164cc0736
5677 <cincoseissietesiete@gmail.com>
2025-11-15 17:41:46 -0300

    BD

--- db.py
+++ db.py
@@ -13,3 +13,12 @@ DB_CONFIG = {
 
 def get_connection():
     return pymysql.connect(**DB_CONFIG)
+
+if __name__ == "__main__":
+    # Prueba de conexión
+    try:
+        conn = get_connection()
+        print("Conexión exitosa a la base de datos.")
+        conn.close()
+    except Exception as e:
+        print(f"Error al conectar a la base de datos: {e}")
