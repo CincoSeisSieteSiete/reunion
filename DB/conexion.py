374fd2fa80d01954e41e67470d8450333508d66f
5677 <cincoseissietesiete@gmail.com>
2025-11-14 22:44:20 -0300

    ahhh

--- DB/conexion.py
+++ DB/conexion.py
@@ -3,18 +3,7 @@ from pymysql.cursors import DictCursor
 import os
 from datetime import datetime
 import logging
-"""
-# Configuración de la base de datos
-DB_CONFIG = {
-    'host': os.getenv('DB_HOST', 'localhost'),
-    'user': os.getenv('DB_USER', 'root'),
-    'password': os.getenv('DB_PASSWORD', ''),
-    'database': os.getenv('DB_NAME', 'asistencias_db'),
-    'charset': 'utf8mb4',
-    'cursorclass': DictCursor
-}
 
-"""
 DB_CONFIG = {
     'host': 'nioyfp.mysql.pythonanywhere-services.com',
     'user': 'nioyfp',
