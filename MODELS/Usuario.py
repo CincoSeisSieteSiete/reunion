19ec46520b2a0455e82c1b1135adee6f2c286085
Gatoprogramador888 <143905112+Gatoprogramador888@users.noreply.github.com>
2025-11-16 20:17:25 -0600

    solucionando error de seguridad de dar todos los cumpleaños a solo dar los de un solo grupo

--- MODELS/Usuario.py
+++ MODELS/Usuario.py
@@ -9,3 +9,27 @@ class Usuario:
     
     def to_tuple(self):
         return (self.id, self.nombre, self.email, self.password, self.fecha_nacimiento, self.rol_id)
+    
+    
+class UsuarioConfigurable:
+    def __init__(self, nombre, email, password, fecha_nacimiento):
+        self.nombre = nombre
+        self.email = email
+        self.password = password
+        self.fecha_nacimiento = fecha_nacimiento
+        
+    def to_tuple(self):
+        return (self.nombre, self.email, self.password, self.fecha_nacimiento)
+
+class UsuarioDict:
+    id = 'id'
+    nombre = 'nombre'
+    email = 'email'
+    password = 'password'
+    fecha_nacimiento = 'fecha_nacimiento'
+    rol_id = 'rol_id'
+    puntos = 'puntos'
+    racha = 'racha'
+    fecha_registro = 'fecha_registro'
+    reset_token = 'reset_token'
+    reset_expires = 'reset_expires'
