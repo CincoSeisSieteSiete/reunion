ee26056478927100d0c7427a606f49c60de34c27
5677 <cincoseissietesiete@gmail.com>
2025-11-27 22:05:19 -0300

    Produccion

--- app.py
+++ app.py
@@ -172,5 +172,5 @@ def refresh():
 
 if __name__ == '__main__':
     # Ejecutar aplicación
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run()
     #debug=True, host='0.0.0.0', port=5000
